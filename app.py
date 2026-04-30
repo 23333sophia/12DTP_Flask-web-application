069a4bec877427cae6649cdd93de154e4b3583cd
23333sophia <23333@burnside.school.nz>
2026-04-30 14:43:13 +1200

    made db + connected

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+
+DATABASE = 'bnd.db'
 
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route("/")
-def hello_world():
+def home():
+    #home page
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
